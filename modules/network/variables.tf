bb37ac86deadefb3e99d1d2d69d6590dd3953528
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-10 17:31:35 +0200

    Create variables.tf

---
+++ modules/network/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "network_name" {
+  type = string
+}
+
+variable "subnetwork_name" {
+  type = string
+}
