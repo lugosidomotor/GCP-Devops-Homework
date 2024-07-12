ef7c0301fc6911dddbe1f14e25b5849b03407396
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-12 22:51:45 +0200

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "gke_cluster_name" {
-  description = "The name of the GKE cluster"
-  value       = module.gke.cluster_name
-}
-
-output "bucket_name" {
-  description = "The name of the GCS bucket"
-  value       = module.storage.bucket_name
-}
