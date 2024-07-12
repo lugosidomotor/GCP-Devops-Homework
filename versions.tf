4bb5462b4755992e155cd09a5301b0b1dafb78d4
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 01:23:37 +0200

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,6 +1,11 @@
 terraform {
   required_version = ">= 0.13"
 
+  backend "gcs" {
+    bucket  = "PLACEHOLDER"
+    prefix  = "PLACEHOLDER"
+  }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
