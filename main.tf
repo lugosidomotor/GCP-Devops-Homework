0cd9278a1ad7e1dfb8931920103d26307ac5d2cc
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 10:55:16 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,38 +13,35 @@ data "google_container_cluster" "my_cluster" {
 data "google_client_config" "default" {}
 
 provider "kubernetes" {
-  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-  token = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(
-    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate
-  )
+  host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token                  = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+  load_config_file       = false
 }
 
 provider "helm" {
   kubernetes {
-    host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
-    token = data.google_client_config.default.access_token
-    cluster_ca_certificate = base64decode(
-      data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate
-    )
+    host                   = "https://${data.google_container_cluster.my_cluster.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
 
 module "network" {
-  source         = "./modules/network"
-  project_id     = var.project_id
-  region         = var.region
-  network_name   = var.network_name
+  source          = "./modules/network"
+  project_id      = var.project_id
+  region          = var.region
+  network_name    = var.network_name
   subnetwork_name = var.subnetwork_name
 }
 
 module "gke" {
-  source          = "./modules/gke"
-  project_id      = var.project_id
-  region          = var.region
-  cluster_name    = var.cluster_name
-  network         = module.network.network_name
-  subnetwork      = module.network.subnetwork_name
+  source                = "./modules/gke"
+  project_id            = var.project_id
+  region                = var.region
+  cluster_name          = var.cluster_name
+  network               = module.network.network_name
+  subnetwork            = module.network.subnetwork_name
   service_account_email = module.storage.service_account_email
 }
 
@@ -69,7 +66,7 @@ resource "helm_release" "streamlit" {
     value = "LoadBalancer"
   }
 
-  depends_on = [module.gke, kubernetes_provider]
+  depends_on = [module.gke]
 }
 
 resource "helm_release" "mlflow" {
@@ -88,5 +85,5 @@ resource "helm_release" "mlflow" {
     value = "LoadBalancer"
   }
 
-  depends_on = [module.gke, kubernetes_provider]
+  depends_on = [module.gke]
 }
