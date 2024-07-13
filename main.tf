db8e4940f7ebcf9febd14e2645145d71be6c2b40
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 10:35:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,7 +45,6 @@ module "gke" {
   cluster_name    = var.cluster_name
   network         = module.network.network_name
   subnetwork      = module.network.subnetwork_name
-  node_machine_type = var.node_machine_type
   service_account_email = module.storage.service_account_email
 }
 
