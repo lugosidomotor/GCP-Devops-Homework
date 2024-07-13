54f296346f38b631d0e4cc1329ed65156a5450ae
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 10:43:50 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,11 @@
 output "cluster_name" {
-  value = nonsensitive(module.gke.cluster_name)
+  value = google_container_cluster.primary.name
 }
 
-output "endpoint" {
-  value = nonsensitive(module.gke.endpoint)
+output "cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth[0].cluster_ca_certificate
 }
