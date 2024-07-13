96fed3387c371bab1d8ebd2f74d0f40159d8dec1
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 10:47:34 +0200

    Update outputs.tf

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -2,7 +2,7 @@ output "cluster_name" {
   value = google_container_cluster.primary.name
 }
 
-output "endpoint" {
+output "cluster_endpoint" {
   value = google_container_cluster.primary.endpoint
 }
 
