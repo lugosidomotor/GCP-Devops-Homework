d20dfc4e2a6aa59e11a75fcc5a83dfcd1f5beb71
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-07-13 10:16:38 +0200

    Update main.tf

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -17,7 +17,7 @@ resource "google_container_node_pool" "primary_nodes" {
 
   node_config {
     preemptible  = false
-    machine_type = var.node_machine_type
+    machine_type = var.gpu_node_required ? var.gpu_node_type : var.default_node_type
 
     service_account = google_service_account.sa.email
     oauth_scopes = [
